8df0b7ab2f8b73ed3478921fcdb2b5063c4c497b
Sachin Sharma <sachinkrsharmaa@gmail.com>
2023-02-12 14:47:01 +0530

    removed hamburger menu

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -5,9 +5,7 @@ const Navbar = () => {
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container">
                 <Link href='/' className="navbar-brand"><span id="logo">stipill</span> <span id="careers">Jobs</span> </Link>
-                <button className="navbar-toggler border-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
+                
             </div>
         </nav>
     );
